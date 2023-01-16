33ac55e93d4b84bf6aa4cf1b329d84e17f07229a
jerrygerou <jerrysgerou@gmail.com>
2023-01-16 15:51:39 -0600

    Add basic components

---
+++ frontend_react/src/containers/Art/Art.jsx
@@ -0,0 +1,16 @@
+import { AppWrap, MotionWrap } from '../../wrapper';
+import './Art.scss';
+
+const Art = () => {
+    return (
+        <>
+            <h2 className="head-text">I Make art!</h2>
+        </>
+    );
+}
+
+export default AppWrap(
+    MotionWrap(Art, 'app__art'),
+    'art',
+    'app__whitebg',
+);
