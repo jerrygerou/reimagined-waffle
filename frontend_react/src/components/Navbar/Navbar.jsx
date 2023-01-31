a48e54d16b69c0f79740076ba8287f4aca41b612
jerrygerou <jerrysgerou@gmail.com>
2023-01-30 20:08:35 -0600

    Render different App components depending on window location

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
                 {['home', 'about', 'work', 'skills', 'art', 'contact'].map((item) => (
                     <li className="app__flex p-text" key={`link-${item}`}>
                         <div />
-                        <a href={`#${item}`}>{item}</a>
+                        <a href={`/#${item}`}>{item}</a>
                     </li>
                 ))}
             </ul>
@@ -36,7 +36,7 @@ const Navbar = () => {
                         <ul>
                             {['home', 'about', 'work', 'skills', 'art', 'contact'].map((item) => (
                                 <li key={item}>
-                                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                                    <a href={`/#${item}`} onClick={() => setToggle(false)}>
                                         {item}
                                     </a>
                                 </li>
--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -1,9 +1,11 @@
 import NavigationDots from './NavigationDots';
 import SocialMedia from './SocialMedia';
 import Navbar from './Navbar/Navbar';
+import Blog from './Blog/Blog';
 
 export {
     NavigationDots,
     SocialMedia,
     Navbar,
+    Blog
 };
