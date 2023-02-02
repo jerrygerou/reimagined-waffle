8b86998c08fb642f527c80469aed00f0d431325d
jerrygerou <jerrysgerou@gmail.com>
2023-02-02 12:30:57 -0600

    Add blog schema

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -6,6 +6,10 @@ import experiences from './experiences.js'
 import skills from './skills.js'
 import workExperience from './workExperience.js'
 import works from './works.js'
+import author from './blog/author.js'
+import blockContent from './blog/blockContent.js'
+import category from './blog/category.js'
+import post from './blog/post.js'
 
 export const schemaTypes = [
   abouts,
@@ -14,5 +18,9 @@ export const schemaTypes = [
   experiences,
   skills,
   workExperience,
-  works
+  works,
+  author,
+  blockContent,
+  category,
+  post
 ]
