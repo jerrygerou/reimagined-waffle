39bf9af3d199933bc6f8ef6d9d8d783699d9a6f1
jerrygerou <jerrysgerou@gmail.com>
2023-02-21 10:36:17 -0600

    Add link and icon for blogger

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { FaBlogger } from 'react-icons/fa';
 
 const SocialMedia = () => (
     <div className="app__social">
@@ -11,6 +12,11 @@ const SocialMedia = () => (
         <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/in/jerrygerou/'>
             <div>
                 <BsLinkedin />
+            </div>
+        </a>
+        <a target='_blank' rel="noreferrer" href='http://blog.jerrygerou.com/'>
+            <div>
+                <FaBlogger />
             </div>
         </a>
     </div>
